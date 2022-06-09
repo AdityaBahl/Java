e1e9f3911030785c364291a4429469c611d14dc9
AdityaBahl <adityabahl12345@gmail.com>
2022-06-09 21:59:37 +0530

    Third file

---
+++ stringbuffer.java
@@ -0,0 +1,18 @@
+public class stringbuffer {
+    public static void main(String[] args) {
+        // stringbuffer is a peer class of string that provides growable and writable
+        // character sequence
+
+        // current length of stringbuffer can be found via the length() method, while
+        // the total allocated capacity can be found by capacity() method
+
+        // insert() - StringBuffer insert(int index, Object(String,int etc) obj(name))
+        // .reverse() - reverses the string buffer
+
+        // Wrapper class - converts primitive data types into objects
+        // Examples - Integer.toString(a) // converts int a to string object
+        // Long.parseLong(str) - converting string str to long
+        // Double.valueOf(str) - converts string to double object
+
+    }
+}
