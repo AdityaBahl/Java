e4d85d561579560dc4739019e53df5f1d0ff4210
AdityaBahl <adityabahl12345@gmail.com>
2022-06-10 13:58:02 +0530

    OOPS 2 updated

---
+++ Constructors.java
@@ -0,0 +1,40 @@
+class box {
+    double width;
+    double height;
+    double depth;
+
+    box() {
+        width = 0.0;
+        height = 0.0;
+        depth = 0.0;
+    }
+
+    box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    void setDim(double w, double h, double d) {// parameterized
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+public class Constructors {
+    public static void main(String[] args) {
+        box mybox1 = new box();
+        System.out.println(mybox1.width);
+        System.out.println(mybox1.depth);
+        System.out.println(mybox1.height);
+        box mybox2 = new box(20.0, 30.0, 40.0);
+        System.out.println(mybox2.width);
+        System.out.println(mybox2.depth);
+        System.out.println(mybox2.height);
+    }
+}
