9f528e308c06cf9dc894493d3c2e1afdb233b0c0
AdityaBahl <adityabahl12345@gmail.com>
2022-06-14 22:07:13 +0530

    Program for Recursion

---
+++ Recursion.java
@@ -0,0 +1,14 @@
+class Factorial {
+    int fact(int n) {
+        if (n == 1)
+            return 1;
+        return fact(n - 1) * n;
+    }
+}
+
+public class Recursion {
+    public static void main(String args[]) {
+        Factorial fc = new Factorial();
+        System.out.println("Factorial of 4 is: " + fc.fact(4));
+    }
+}
