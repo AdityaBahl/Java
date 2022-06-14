312509f54f81b8a144e535e8a26eaddd7faa5927
AdityaBahl <adityabahl12345@gmail.com>
2022-06-14 22:09:07 +0530

    Program for Recursion

--- Recursion.java
+++ Recursion.java
@@ -9,8 +9,8 @@ class Factorial {
 public class Recursion {
     public static void main(String args[]) {
         Factorial fc = new Factorial();
-        System.out.println("Factorial of 4 is: " + fc.fact(3));
-        System.out.println("Factorial of 5 is: " + fc.fact(4));
+        System.out.println("Factorial of 1 is: " + fc.fact(1));
+        System.out.println("Factorial of 2 is: " + fc.fact(2));
         System.out.println("Factorial of 5 is: " + fc.fact(5));
         System.out.println("Factorial of 5 is: " + fc.fact(4));
         System.out.println("Factorial of 5 is: " + fc.fact(5));
