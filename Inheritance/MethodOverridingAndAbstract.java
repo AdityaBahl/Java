ddfd1dca52bf3b5f6d5df2a5f342d0dc643acf31
AdityaBahl <adityabahl12345@gmail.com>
2022-06-19 00:58:52 +0530

    Method Overriding and Abstract Keyword

--- Inheritance/MethodOverridingAndAbstract.java
+++ Inheritance/MethodOverridingAndAbstract.java
@@ -1,5 +1,21 @@
+abstract class A {
+    abstract void callme();// abstract method
+
+    void callmetoo() {
+        System.out.println("concrete method.");
+    }
+}
+
+class B extends A {// Concrete class
+    void callme() {// Implements method
+        System.out.println("B's implementation of callme() function");
+    }
+}
+
 public class MethodOverridingAndAbstract {
     public static void main(String args[]) {
-        A a = new B();// Reference of Abstract
+        A a = new B();// Reference of Abstract super to object of B
+        a.callme();
+        a.callmetoo();
     }
 }
