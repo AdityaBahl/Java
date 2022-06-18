068b1bb2987b8642768202c9f74d31dce90f9bb6
AdityaBahl <adityabahl12345@gmail.com>
2022-06-19 01:44:00 +0530

    Final Keyword

--- Inheritance/FinalKeyword.java
+++ Inheritance/FinalKeyword.java
@@ -1,7 +1,10 @@
 //Final Keyword has 3 uses:
 //1.declare a value whose value cant be changed after initialisation
 //2.declare a method which can not be overridden
-//3.declare a class which can not have any subclass
+//3.declare a class which can not have any subclass(can not be inherited)
+
+//declaring a class final implicitly declares all its methods final(its not neccessary then to declare them finally)
+//illegal to declare a class as both abstract and final.
 class A {
     final void meth() {// Final method
         System.out.println("This is a final method.");
