4f00650bb2d001bca83d162682e78e9437129c4d
AdityaBahl <adityabahl12345@gmail.com>
2022-06-18 13:48:51 +0530

    Single Inheritance

--- Inheritance/Single.java
+++ Inheritance/Single.java
@@ -19,14 +19,18 @@ class B extends A {
 }
 
 public class Single {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         A superObj = new A();
         B subObj = new B();
-        superObj.i=10;//initialize class A's Variable
+        superObj.i = 10;// initialize class A's Variable
         System.out.println("Contents of a: ");
         superObj.showi();
-        supObj.i=7;
-        subObj.j=8;
-        S
+        subObj.i = 7;
+        subObj.j = 8;
+        System.out.println("Contents of b: ");
+        subObj.showi();
+        subObj.showj();
+        System.out.println("Sum of i and j in b: ");
+        subObj.sum();
     }
 }
