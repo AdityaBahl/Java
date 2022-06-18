971b036ac1f62e5961c16d790319148b0062230b
AdityaBahl <adityabahl12345@gmail.com>
2022-06-18 23:17:15 +0530

    Multi-Level Inheritance

--- Inheritance/MultiLevel.java
+++ Inheritance/MultiLevel.java
@@ -1,7 +1,24 @@
-//first super class constructor is called then sub class
+class A {// Super class
+    A() {
+        System.out.println("Inside A's Constructor");
+    }
+}
 
+class B extends A {
+    B() {
+        System.out.println("Inside B's Constructor");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Inside C's Constructor");
+    }
+}
+
+// first super class constructor is called then sub class
 public class MultiLevel {
     public static void main(String args[]) {
-
+        C obj = new C();// object of class C
     }
 }
