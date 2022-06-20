3353677736d0647190f7c6d67e5e7851bb1f63d5
AdityaBahl <adityabahl12345@gmail.com>
2022-06-20 22:11:06 +0530

    Exception Handling Explanation Program

--- Exception_Handling/Explanation.java
+++ Exception_Handling/Explanation.java
@@ -1,10 +1,16 @@
-import java.util.*;
+import java.util.*;//reads integer from user
 
 public class Explanation {
     public static void main(String args[]) {
         System.out.println("Enter an Integer");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("Thank you for entering the data");
+        try {
+            int a = sc.nextInt();// iF user enters invalid format?
+            System.out.println("Thank you for entering the data");
+        } catch (Exception e)// catch the error object
+        {
+            System.out.println("Error in input: " + e);
+        }
+        System.out.println("After try/catch blocks");
     }
 }
