9c2123975328ae32aa408fd6e56e29816a747b29
AdityaBahl <adityabahl12345@gmail.com>
2022-06-20 22:58:10 +0530

    Multiple Catch Clause

--- Exception_Handling/MultipleCatch.java
+++ Exception_Handling/MultipleCatch.java
@@ -1,6 +1,15 @@
 public class MultipleCatch {
     // Each catch catches a different exception
+    // Order is :- generic<specialised excpetions
+    // example - first ArithmeticExcpetion, then Excpetion
+    // if vice versa then compile time error
+    // after one catch, others are bypassed
     public static void main(String[] args) {
-
+        try{
+            int a=0;// value of a can also be greater than 0
+            System.out.println("a= "+a);
+            int b=42/a;//Divide by 0
+            
+        } 
     }
 }
