da9eccb3be77f778d3224ace1a2ddc08363c081d
AdityaBahl <adityabahl12345@gmail.com>
2022-06-20 22:07:51 +0530

    Exception Handling Explanation Program

--- Exception_Handling/Explanation.java
+++ Exception_Handling/Explanation.java
@@ -3,5 +3,8 @@ import java.util.*;
 public class Explanation {
     public static void main(String args[]) {
         System.out.println("Enter an Integer");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Thank you for entering the data");
     }
 }
