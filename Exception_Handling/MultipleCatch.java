79e568136a4d1807d4af8bf85d4b05c53647f439
AdityaBahl <adityabahl12345@gmail.com>
2022-06-20 23:00:58 +0530

    Multiple Catch Clause

--- Exception_Handling/MultipleCatch.java
+++ Exception_Handling/MultipleCatch.java
@@ -5,12 +5,19 @@ public class MultipleCatch {
     // if vice versa then compile time error
     // after one catch, others are bypassed
     public static void main(String[] args) {
-        try{
-            int a=0;// value of a can also be greater than 0
-            System.out.println("a= "+a);
-            int b=42/a;//Divide by 0
-            int c[]=new int[5];
-            c[8]=99;//Array index out of bound
-        } 
+        try {
+            int a = 0;// value of a can also be greater than 0
+            System.out.println("a= " + a);
+            int b = 42 / a;// Divide by 0
+            int c[] = new int[5];
+            c[8] = 99;// Array index out of bound
+        } catch (ArithmeticException e)// Inherited from java.lang.RuntimeException
+        {
+            System.out.println("Divide by 0: " + e);
+        } catch (ArrayIndexOutOfBoundsException e)// RunExp->IndexOB->AIOB
+        {
+            System.out.println("Array index oob: " + e);
+        }
+        System.out.println("After try/catch blocks.");
     }
 }
