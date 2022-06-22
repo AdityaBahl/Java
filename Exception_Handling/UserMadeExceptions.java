f3afa0667210b9aff77939ee3f8105109df2eeac
AdityaBahl <adityabahl12345@gmail.com>
2022-06-23 00:46:26 +0530

    Exceptions made by User

--- Exception_Handling/UserMadeExceptions.java
+++ Exception_Handling/UserMadeExceptions.java
@@ -17,8 +17,9 @@ class Myclass {
     void compute(int a)// throws MyException
     {
         System.out.println("Called compute(" + a + ")");
-        if (a > 10)
+        if (a > 10) {
             throw new MyException(a);
+        }
         System.out.println("Normal Exit");
     }
 }
