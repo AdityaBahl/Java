a1ecee9a6018e2bbf11b5716c853fdc31573ef48
AdityaBahl <adityabahl12345@gmail.com>
2022-06-23 00:20:03 +0530

    Exceptions made by User

--- Exception_Handling/UserMadeExceptions.java
+++ Exception_Handling/UserMadeExceptions.java
@@ -1,6 +1,21 @@
-//steps - 1. create your own exception class
-//2. Use it to throw the exception object
-//3. Catch the thrown Object
+//create your own exception
+class MyException extends Exception {
+    private int detail;// instance variable
+
+    MyException(int a)// constructor
+    {
+        detail = a;// assign value to instance variable
+    }
+
+    public String toString(){
+        return "MyException[" +detail+ "]"
+    }
+}
+// Use it to throw the exception object
+
+// steps - 1. create your own exception class
+// 2. Use it to throw the exception object
+// 3. Catch the thrown Object
 public class UserMadeExceptions {
     public static void main(String[] args) {
 
