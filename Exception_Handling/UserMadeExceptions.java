2e858407e20fb26b02adb8c596d2f6fba4174c91
AdityaBahl <adityabahl12345@gmail.com>
2022-06-23 00:14:22 +0530

    Exceptions made by User

--- Exception_Handling/UserMadeExceptions.java
+++ Exception_Handling/UserMadeExceptions.java
@@ -1,6 +1,6 @@
 //steps - 1. create your own exception class
 //2. Use it to throw the exception object
-//3. 
+//3. Catch the thrown Object
 public class UserMadeExceptions {
     public static void main(String[] args) {
 
