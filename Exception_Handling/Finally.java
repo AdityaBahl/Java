5ec6557a56238484608edd3148d446627bd64486
AdityaBahl <adityabahl12345@gmail.com>
2022-06-22 23:09:05 +0530

    Finally(Used to execute code irrespective of exception) Keyword

--- Exception_Handling/Finally.java
+++ Exception_Handling/Finally.java
@@ -3,11 +3,23 @@
 //try->catch->finally or try->finally
 public class Finally {
     public static void main(String[] args) {
+        System.out.println("Encountering Exception");
         try {
             int arr[] = new int[10];
             int i = 12;
             System.out.println(arr[i]);// Results in Error here
-        } catch (ArrayIndexOutOfBoundException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();// Use of method associated with "e"
+        } finally {// Must be either after Catch OR try(if catch is not present)
+            System.out.println("finally");
+        }
+        System.out.println("after finally\n");
+        System.out.println("When no Exception Encountered");
+        try {
+            int arr[] = new int[10];
+            int i = 12;
+            System.out.println(arr[i]);// ArrayIndexOutOfBoundsException
+        } catch (ArithmeticException e) {// Not Encountered
             e.printStackTrace();// Use of method associated with "e"
         } finally {// Must be either after Catch OR try(if catch is not present)
             System.out.println("finally");
