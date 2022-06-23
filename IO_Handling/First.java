a7a76fc043d502eca6f3f67abce39a4ab18db889
AdityaBahl <adityabahl12345@gmail.com>
2022-06-23 18:24:39 +0530

    Notes and program on Input/Output Handling

---
+++ IO_Handling/First.java
@@ -0,0 +1,14 @@
+//Notes-
+//Some of the methods available in input stream classes are - 
+// 1. int available() - provides no of bytes available in input
+// 2. int read() - reads the next byte of data from the input stream
+// 3. int read(byte b[]) - Reads b.length(max) bytes and stores them into array b
+// 4. int read(byte b[],int off,int len) - Reads upto len bytes of data from the input stream into array b
+// 5. void reset() - reset the stream position 
+// 6. long skip(long n) - skips n byte of bytes
+//
+public class First{
+    public static void main(String args[]){
+
+    }
+}
