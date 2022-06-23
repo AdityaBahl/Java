914126b2e1b4d9f5d9bfd41d5176742af5b71d73
AdityaBahl <adityabahl12345@gmail.com>
2022-06-23 18:35:04 +0530

    Notes and program on Input/Output Handling

--- IO_Handling/First.java
+++ IO_Handling/First.java
@@ -6,9 +6,18 @@
 // 4. int read(byte b[],int off,int len) - Reads upto len bytes of data from the input stream into array b
 // 5. void reset() - reset the stream position 
 // 6. long skip(long n) - skips n byte of bytes
-//
-public class First{
-    public static void main(String args[]){
+// 7. void close() - close the stream
+
+//Some of the methods available in output stream classes are - 
+// 1. void write(int b) - writes b to output stream
+// 2. void write(byte b[]) - writes b.length bytes to this output stream
+// 3. void write(byte b[],int off, int len) - writes len bytes from b starting at offset off to this output stream
+// 4. void flush() - flushes buffered output bytes to be written out
+// 5. void close() - close the stream
+
+//There are 2 types of streams - byte and character stream
+public class First {
+    public static void main(String args[]) {
 
     }
 }
