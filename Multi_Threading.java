02b9517c33bd414e804ad9330271fe74236ba80b
AdityaBahl <adityabahl12345@gmail.com>
2022-06-24 14:46:32 +0530

    MultiThreading

---
+++ Multi_Threading.java
@@ -0,0 +1,17 @@
+//this program demonstrates main thread
+//main thread is a thread that begins as soon as a program starts
+public class Multi_Threading {
+    public static void main(String args[]) {
+        Thread t = Thread.currentThread();// Handle to this thread
+        System.out.println("Current thread: " + t);
+        // After name change:Thread[My Thread,5,main]
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);// sleep 1000 ms
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+    }
+}
