05a194df59420c240bcfae75428684135e108d89
AdityaBahl <adityabahl12345@gmail.com>
2022-06-24 20:05:28 +0530

    Multi_Threading2

---
+++ Multi_Threading2.java
@@ -0,0 +1,15 @@
+public class Multi_Threading2 {
+    public static void main(String[] args) {
+        //create multiple threads
+        NewThread t1= new NewThread("One");//Thread "One"
+        NewThread t2= new NewThread("Two");//Thread "Two"
+        NewThread t3= new NewThread("Three");//Thread "three"
+        try{
+            Thread.sleep(10000);//sleep for 10000ms or 10 secs
+        }
+        catch(InterruptedException e){
+            System.out.println("Main Thread Interrupted");
+        }
+        System.out.println("Main Thread Exiting");
+    }
+}
