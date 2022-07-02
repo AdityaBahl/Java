c8f6d5cf2de10a01484ce0a7db2329049805e74b
AdityaBahl <adityabahl12345@gmail.com>
2022-07-02 18:55:08 +0530

    Rectangle by Generic

--- Generic_Programming/Rectangle.java
+++ Generic_Programming/Rectangle.java
@@ -1,24 +1,36 @@
-class RectGen<T>{//single type parameter T
+class RectGen<T> {// single type parameter T
     private T method;
     private T width;
-    RectGen(T l, T w){
-        length=l;
-        width=w;
+
+    RectGen(T l, T w) {
+        length = l;
+        width = w;
     }
-    T getLength(){
+
+    T getLength() {
         return length;
     }
-    T getWidth(){
+
+    T getWidth() {
         return width;
     }
-    T calculateArea(){
-        return length*width;
+
+    T calculateArea() {
+        return length * width;
     }
 }
-public class Rectangle{
-    public static void main(String args[]){
-        //Rectangle of Integer(Autoboxing and Unboxing)
-        RectGen<Integer>obj1=new RectGen<Integer>(10,20);
-        
+
+public class Rectangle {
+    public static void main(String args[]) {
+        // Rectangle of Integer(Autoboxing and Unboxing)
+        RectGen<Integer> obj1 = new RectGen<Integer>(10, 20);
+        int iwidth = obj1.getLength();// Auto Unboxing
+        int ilength = obj1.getWidth();
+        int area = obj1.calculateArea();
+        System.out.println("Length= " + ilength + " Width= " + iwidth);
+        System.out.println("Area= " + ilength * iwidth);// imp to note
+        System.out.println("Area= " + area);
+
+        // Rec
     }
 }
