ee6e7c4e00b3ff6ca3fa806ad490aff0935d1c37
AdityaBahl <adityabahl12345@gmail.com>
2022-07-02 18:40:12 +0530

    Rectangle by Generic

--- Generic_Programming/Rectangle.java
+++ Generic_Programming/Rectangle.java
@@ -1,5 +1,20 @@
+class RectGen<T>{//single type parameter T
+    private T method;
+    private T width;
+    RectGen(T l, T w){
+        length=l;
+        width=w;
+    }
+    T getLength(){
+        return length;
+    }
+    T getWidth(){
+        return width;
+    }
+
+}
 public class Rectangle{
     public static void main(String args[]){
-        
+
     }
 }
