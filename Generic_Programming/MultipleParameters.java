badc1fe5bd1c0c64ae4996b4e24ba5be7a2edd44
AdityaBahl <adityabahl12345@gmail.com>
2022-07-02 19:22:29 +0530

    Multiple Parameters

--- Generic_Programming/MultipleParameters.java
+++ Generic_Programming/MultipleParameters.java
@@ -1,3 +1,16 @@
+class GenMulti<T,V,W>{
+    T d1;//Object reference
+    V d2;
+    W d3;
+    GenMulti(T v1,V v2,W v3){
+    d1=v1;
+    d2=v2;
+    d3=v3;
+    }
+    //display type of objects
+    void display type
+}
+
 public class MultipleParameters {
 
 }
