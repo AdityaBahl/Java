2614606e4ca34ee0a68207225889f4c4b9dba21b
AdityaBahl <adityabahl12345@gmail.com>
2022-07-03 14:00:36 +0530

    MultiThreading By Code with Harry By Extending Thread Class

--- MultiThreading_cwh.java
+++ MultiThreading_cwh.java
@@ -1,7 +1,11 @@
 // observe that both operations run concurrently and randomly and not subsequently
 // class Mythread1 extends Thread
 
+////////////////////////////////
+
 //Made By Extending Thread Class
+
+////////////////////////////////
 
 class Mythread1 extends Thread {
     // @Override
