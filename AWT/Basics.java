afce73ce2dcb91c2b405c9785f0d1a49d6d15afe
AdityaBahl <adityabahl12345@gmail.com>
2022-07-03 19:33:20 +0530

    Basics of AWT

--- AWT/Basics.java
+++ AWT/Basics.java
@@ -11,3 +11,18 @@ public class Basics extends Frame {
 
     }
 }
+//////////////////////////////////////
+
+// Alternate way:-
+// public class Basics extends Frame {
+// public Basics() {
+// setSize(400, 300);
+// setVisible(true);
+// }
+// public static void main(String[] args) {
+// Basics b = new Basics();
+//
+// }
+// }
+
+//////////////////////////////////////
