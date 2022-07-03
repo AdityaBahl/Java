dcae20ed960c59773269f4ff37f295e43cb339b8
AdityaBahl <adityabahl12345@gmail.com>
2022-07-03 14:24:28 +0530

    MultiThreading By Code with Harry By Implementing Runnable Interface

--- MultiThreading_cwh_RunnableInterface.java
+++ MultiThreading_cwh_RunnableInterface.java
@@ -1,21 +1,24 @@
 class Mythread3 implements Runnable {
-    public void run(){
+    public void run() {
         System.out.println("I am a thread 1");
     }
 }
+
 class Mythread4 implements Runnable {
-    public void run(){
+    public void run() {
         System.out.println("I am a thread 2");
     }
 }
+
 public class MultiThreading_cwh_RunnableInterface {
     public static void main(String args[]) {
-        Mythread3 bullet1= new Mythread3();//bullet in gun analogy
-        Thread gun1=new Thread(bullet1);        
-        Mythread4 bullet2= new Mythread4();
-        Thread gun2=new Thread(bullet2);
-        gun1.start(); //cant be run directly while implementing Runnable Interface without Thread gun type statements, as in the case of extending Thread class
-        gun2.start(); //cant be run directly while implementing Runnable Interface without Thread gun type statements, as in the case of extending Thread class 
-    
-      }
+        Mythread3 bullet1 = new Mythread3();// bullet in gun analogy
+        Thread gun1 = new Thread(bullet1);
+        Mythread4 bullet2 = new Mythread4();
+        Thread gun2 = new Thread(bullet2);
+        gun1.start(); // cant be run directly while implementing Runnable Interface without Thread gun
+                      // type statements, as in the case of extending Thread class
+        gun2.start(); // cant be run directly while implementing Runnable Interface without Thread gun
+                      // type statements, as in the case of extending Thread class
+    }
 }
