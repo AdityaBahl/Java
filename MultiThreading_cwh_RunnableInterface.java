7cc9e560d97aee2eb9f64b086dcaa3a035cf7aa3
AdityaBahl <adityabahl12345@gmail.com>
2022-07-03 14:08:27 +0530

    MultiThreading By Code with Harry By Implementing Runnable Interface

--- MultiThreading_cwh_RunnableInterface.java
+++ MultiThreading_cwh_RunnableInterface.java
@@ -1,9 +1,18 @@
 class MyThread1 implements Runnable {
-
+    public void run(){
+        System.out.println("I am a thread 1");
+    }
 }
-
+class MyThread2 implements Runnable {
+    public void run(){
+        System.out.println("I am a thread 2");
+    }
+}
 public class MultiThreading_cwh_RunnableInterface {
     public static void main(String args[]) {
-
+        Mythread1 t1= new Mythread1();
+        Mythread1 t2= new Mythread2();
+       // t1.start(); //cant be run directly while implementing Runnable Interface, as in the case of extending Thread class
+       // t2.start(); //cant be run directly while implementing Runnable Interface, as in the case of extending Thread class 
     }
 }
