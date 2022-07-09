95f800b934ea928026ede3301c618bf113396fbe
AdityaBahl <adityabahl12345@gmail.com>
2022-07-10 03:21:21 +0530

    Source File

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,9 +1,15 @@
 package java.practice.Calculator;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class Main {
+//import java.awt.*;
+public class Main extends JFrame {
+    public Main() {
+        setBounds(100, 90, 250, 40);
+        setLayout(null);
+
+    }
+
     public static void main(String args[]) {
 
     }
