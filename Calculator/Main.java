299231a3e89b9b2af7bcfe4ca4a507043684d306
AdityaBahl <adityabahl12345@gmail.com>
2022-07-10 12:38:48 +0530

    Source File

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -2,15 +2,22 @@ package java.practice.Calculator;
 
 import javax.swing.*;
 
-//import java.awt.*;
-public class Main extends JFrame {
-    public Main() {
-        setBounds(100, 90, 250, 40);
-        setLayout(null);
-
-    }
-
+public class Main {
     public static void main(String args[]) {
-
+        JFrame jframe = new JFrame();
+        jframe.setTitle("This is a normal frame");
+        jframe.setLayout(null);
+        jframe.setSize(400, 400);
+        jframe.setVisible(true);
     }
 }
+// import java.awt.*;
+// class Calculator extends JFrame {
+// public static void calc() {
+// setBounds(100, 90, 250, 40);
+// setTitle("Calculator");
+// setLayout(null);
+// setVisible(true);
+// setSize(500, 500);
+// }
+// }
