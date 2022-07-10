1f2898195ce63c01010c90e741e899995e65d8a5
AdityaBahl <adityabahl12345@gmail.com>
2022-07-10 18:19:10 +0530

    Main File

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -2,15 +2,58 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String args[]) {
-        //buttons
-        JButton jb1 = new JButton("OG");
-        jb1.setBounds(10,100,100,50);
-        JButton jb2 = new JButton("OG");
-        jb2.setBounds(120,100,100,50);
-        JButton jb3 = new JButton("OG");
-        jb3.setBounds(230,100,100,50);
-        JButton jb4 = new JButton("OG");
-        jb4.setBounds(340,100,100,50);
+        //BUTTONS
+
+        //1st Line
+        JButton og = new JButton("O G");
+        og.setBounds(10,100,100,50);
+        JButton mod = new JButton("%");
+        mod.setBounds(131,100,100,50);
+        JButton power = new JButton("^");
+        power.setBounds(252,100,100,50);
+        JButton undrrt = new JButton("sqr root");
+        undrrt.setBounds(373,100,100,50);
+
+        //2nd Line
+        JButton one = new JButton("1");
+        one.setBounds(10,160,100,50);
+        JButton two = new JButton("2");
+        two.setBounds(131,160,100,50);
+        JButton three = new JButton("3");
+        three.setBounds(252,160,100,50);
+        JButton plus = new JButton("+");
+        plus.setBounds(373,160,100,50);
+
+        //3rd Line
+        JButton four = new JButton("4");
+        four.setBounds(10,220,100,50);
+        JButton five = new JButton("5");
+        five.setBounds(131,220,100,50);
+        JButton six = new JButton("6");
+        six.setBounds(252,220,100,50);
+        JButton minus = new JButton("-");
+        minus.setBounds(373,220,100,50);
+
+        //4th Line
+        JButton seven = new JButton("7");
+        seven.setBounds(10,280,100,50);
+        JButton eight = new JButton("8");
+        eight.setBounds(131,280,100,50);
+        JButton nine = new JButton("9");
+        nine.setBounds(252,280,100,50);
+        JButton divide = new JButton("/");
+        divide.setBounds(373,280,100,50);
+
+        //5th Line
+        JButton zero = new JButton("0");
+        zero.setBounds(10,340,100,50);
+        JButton  equalsto= new JButton("=");
+        equalsto.setBounds(131,340,220,50);
+        JButton multiply = new JButton("x");
+        multiply.setBounds(373,340,100,50);
+        //JButton og = new JButton("O G");
+        //og.setBounds(373,100,100,50);
+
         //JButton jb = new JButton("OG");
         //jb.setBounds(10,100,100,50);
         //JButton jb = new JButton("OG");
@@ -18,19 +61,45 @@ public class Main {
         
         //JLabel jl=new JLabel("Calculator");
         JTextField jtf = new JTextField("CALCULATE!");
-        jtf.setBounds(10, 20, 460, 70);
+        jtf.setBounds(10, 20, 465, 70);
         JFrame jframe = new JFrame();
         // jframe.setBounds(100, 90, 250, 40);
         jframe.add(jtf);
         //jframe.add(jl);
-        jframe.add(jb1);
-        jframe.add(jb2);
-        jframe.add(jb3);
-        jframe.add(jb4);
+        //
+        jframe.add(og);
+        jframe.add(mod);
+        jframe.add(power);
+        jframe.add(undrrt);
+        //
+        jframe.add(one);
+        jframe.add(two);
+        jframe.add(three);
+        jframe.add(plus);
+        //
+        jframe.add(four);
+        jframe.add(five);
+        jframe.add(six);
+        jframe.add(minus);
+        //
+        jframe.add(seven);
+        jframe.add(eight);
+        jframe.add(nine);
+        jframe.add(divide);
+        //
+        jframe.add(zero);
+        jframe.add(equalsto);
+        jframe.add(multiply);
+        //jframe.add(og);
+        //
+        //jframe.add(e);
+        //jframe.add(o);
+        //jframe.add(ree);
+        //jframe.add();
         jframe.setTitle("Calculator");
         jframe.setLayout(null);
         jframe.setVisible(true);
-        jframe.setSize(500, 500);
+        jframe.setSize(500, 450);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
